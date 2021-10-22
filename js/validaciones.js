495e11c85412013d4d67fb960d9145a47ad8643d
devchrisar <christopher.ariasar@amigo.edu.co>
2021-10-22 16:36:58 -0500

    <feat> : Added sprint 4

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,11 +1,14 @@
 (function () {
     "use strict";
+    //!sprint 2 
     var errorDiv = document.getElementById('error')
+    var inFormDiv = document.getElementById('inForm')
     //campos datos usuarios 
     var nombre = document.getElementById('in_nombre_usuario');
     var pass = document.getElementById('in_contrasena');
     var confirm_pass = document.getElementById('in_confirmar_contrasena');
     const formulario = document.getElementById('form_registrarse');
+    const registroBtn = document.getElementById('btn_registrar_usuario');
     const expresiones = {
         nombre: /^[a-zA-Z0-9]{6,30}$/, // Letras [mayus,minus], numeros, 6 - 30 digitos.
         password:  /^[a-zA-Z0-9]{6,16}$/, // Letras, numeros + 6  digitos.
@@ -13,6 +16,7 @@
     formulario.addEventListener('submit', e => {
         e.preventDefault();
     })
+    registroBtn.addEventListener('click', agregarRegistro)
     
     nombre.addEventListener('keyup', validar_nombre_usuario)
     pass.addEventListener('keyup', validar_contrasena)
@@ -84,6 +88,39 @@
             return true;
         }
     }
+    //!sprint 3
+    let registros = [];
+    function agregarRegistro() {
+        let nombreVal = document.getElementById('in_nombre_usuario').value;
+        let passVal = document.getElementById('in_contrasena').value;
+        let confirm_passVal = document.getElementById('in_confirmar_contrasena').value;
+        var fulltable = "";
+        
+        for (var i = 0; i < registros.length; i++) {
+            fulltable += "<ul><li>" + registros[i].usuario + "</li><li>" + registros[i].contrasena + "</li><li>" + registros[i].confirmar_contrasena + "</li></ul>";   
+        }
+        var nuevoArreglo = {usuario: nombreVal,contrasena: passVal,confirmar_contrasena: confirm_passVal};
+        registros.push(nuevoArreglo)
+        inFormDiv.innerHTML = fulltable;
+    }
+    function OrdenarArreglo(registros) {
+        registros.sort((a, b) => {
+            const aSorting = a.nombreVal.toLowerCase();
+            const bSorting = b.nombreVal.toLowerCase();
+            if (aSorting < bSorting) {
+                return -1;
+            }
+            if (aSorting > bSorting) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    console.log(registros);
+
+    module.exports.registros = registros;
+    module.exports.OrdenarArreglo = OrdenarArreglo;
+    module.exports.agregarRegistro = agregarRegistro;
     module.exports.validar_nombre_usuario = validar_nombre_usuario;
     module.exports.validar_contrasena = validar_contrasena;
     module.exports.confirmar_contrasena = confirmar_contrasena;
